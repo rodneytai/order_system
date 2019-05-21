62b6946b611a154851ad87ab25b382a623925230
Rodney Tai <rodneytai97@gmail.com>
2019-05-21 11:51:12 +0800

    setting up db

---
+++ database/migrations/2019_05_21_032123_customer_info.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CustomerInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('CustomerInfo', function (Blueprint $table) {
+            $table->bigIncrements('cusId', 10);
+            $table->string('cusName', 20);
+            $table->string('cusPhone', 15);
+            $table->string('cusAddress', 50);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('CustomerInfo');
+    }
+}
--- database/migrations/2019_05_21_032137_acc_pass.php
+++ database/migrations/2019_05_21_032137_acc_pass.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class AccPass extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,19 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('cusId', 10);
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-        Schema::table('password_resets', function($table) {
+        //
+        Schema::create('AccPass', function (Blueprint $table) {
+            $table->string('acc')->unique();
+            $table->string('passsword');
             $table->foreign('cusId')
                   ->references('cusId')->on('CustomerInfo')
-                  ->onUpdate('cascade') //更新覆蓋
+                  ->onDelete('cascade');
+            $table->timestamp('created_at')->nullable();
+        });
+        Schema::table('AccPass', function($table) {
+            $table->foreign('cusId')
+                  ->references('cusId')->on('CustomerInfo')
+                  ->onUpdate('cascade'); //更新覆蓋
         });
     }
 
@@ -33,6 +36,7 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
+        Schema::dropIfExists('AccPass');
     }
 }
