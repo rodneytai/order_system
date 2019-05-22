1f74c700b38d0d9ea86ecc3fcb3d42dbc7f64366
Rodney Tai <rodneytai97@gmail.com>
2019-05-22 17:02:29 +0800

    Login page minor adjustments , redirecting to home page after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,12 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/home';
+    protected function authenticated() {
+        if (Auth::check()) {
+            return redirect()->intended('/');
+        }
+    }
 
     /**
      * Create a new controller instance.
@@ -35,5 +41,9 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function username()
+    {
+        return 'username';
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        var count = 2;
+        $('#msg').text(count);
+        setTimeout(function () {
+            count = count -1;
+            window.location = '{{ url('/') }}';
+            $('#msg').text(count);
+        }, 3000);
+    });
+</script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -15,6 +27,7 @@
                     @endif
 
                     You are logged in!
+                    Redirecting in <span id="msg"></span> seconds
                 </div>
             </div>
         </div>
