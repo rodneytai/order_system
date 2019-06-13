19719adf4435a34d75a688bb1927c3b759421b61
Rodney Tai <rodneytai97@gmail.com>
2019-06-13 17:27:07 +0800

    SQL SERVER and MySql config

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -35,7 +35,8 @@ class OrderController extends Controller
                     $orderId = 100000;
                 else
                     $orderId += 1;
-                DB::unprepared('SET IDENTITY_INSERT OrderInfo ON');
+                if (env(DB_CONNECTION) == 'sqlsrv')
+                    DB::unprepared('SET IDENTITY_INSERT OrderInfo ON');
                 $id = DB::table('OrderInfo')
                         ->insertGetId([ 
                             "orderId" => $orderId,
@@ -47,20 +48,23 @@ class OrderController extends Controller
                             "orderCus" => $request->input("customer")
                           ]
                         );
-                DB::unprepared('SET IDENTITY_INSERT OrderInfo OFF');
+                if (env(DB_CONNECTION) == 'sqlsrv')
+                    DB::unprepared('SET IDENTITY_INSERT OrderInfo OFF');
                 $dId = DB::table("DeliveryDetails")->max("dId");
                 if ($dId == null)
                     $dId = 800000;
                 else
                     $dId += 1; 
-                DB::unprepared('SET IDENTITY_INSERT DeliveryDetails ON');
+                if (env(DB_CONNECTION) == 'sqlsrv')
+                    DB::unprepared('SET IDENTITY_INSERT DeliveryDetails ON');
                 DB::table("DeliveryDetails")
                   ->insert([
                       "dId" => $dId,
                       "dOrderId" => $id,
                       "dStatus" => "確認訂單中",
                   ]);
-                DB::unprepared('SET IDENTITY_INSERT DeliveryDetails OFF');
+                if (env(DB_CONNECTION) == 'sqlsrv')
+                    DB::unprepared('SET IDENTITY_INSERT DeliveryDetails OFF');
                 $msg = "Success";
             }     
         }
